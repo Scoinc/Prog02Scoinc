3d125eedba6a049fff02f97bfcb8bffa26f02ed1
Scoinc <david_utbult@live.se>
2020-08-28 12:57:22 +0200

    Hello world??

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,18 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas");
+            Console.WriteLine("Enter your name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+            var age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Are you alive? true/false");
+            var alive = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("name: " + name);
+            Console.WriteLine("age: " + age);
+            Console.WriteLine("State of living: " + alive);
         }
     }
 }
