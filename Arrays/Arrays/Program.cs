59534dfe5815f5c4022b6d7a58dc8a21ebee48aa
Scoinc <david_utbult@live.se>
2020-09-07 14:14:46 +0200

    Arrays test

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Integer array with a length of 5 numbers
+            int loops = 8;
+            int[] numbers = new int[loops];
+
+            for(int i = 0; i < loops; i++)
+            {
+                numbers[i] = i;
+                Console.WriteLine(i);
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < loops; i++)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine("\nsum of numbers: " + sum);
+        }
+    }
+}
