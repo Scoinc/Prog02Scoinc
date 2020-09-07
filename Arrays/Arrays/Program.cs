74119b7331a397d091055dd0629ab0455bfdc0f1
Scoinc <david_utbult@live.se>
2020-09-07 14:23:28 +0200

    Array uppgift

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -7,23 +7,20 @@ namespace Arrays
         static void Main(string[] args)
         {
             //Integer array with a length of 5 numbers
-            int loops = 8;
-            int[] numbers = new int[loops];
+            string [] names = new string[5];
 
-            for(int i = 0; i < loops; i++)
+            for(int i = 0; i < names.Length; i++)
             {
-                numbers[i] = i;
-                Console.WriteLine(i);
+                Console.WriteLine("Skriv ett namn: " + i + "/5");
+                names[i] = Console.ReadLine();
             }
 
-            int sum = 0;
+            Console.WriteLine("\n\n\n");
 
-            for (int i = 0; i < loops; i++)
+            for (int i = names.Length - 1; i >= 0; i--)
             {
-                sum += numbers[i];
+                Console.WriteLine(names[i]);
             }
-
-            Console.WriteLine("\nsum of numbers: " + sum);
         }
     }
 }
