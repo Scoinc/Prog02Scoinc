cda6042c95d2a2f3fbb3a43fa75e40877e83c11e
Scoinc <david_utbult@live.se>
2020-09-21 12:33:10 +0200

    e

---
+++ OOPIntro/OOPIntro/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OOPIntro
+{
+    class Person
+    {
+        string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+        int _age;
+
+        Pet _pet;
+
+        public Person(string name, int age, Pet pet)
+        {
+            _name = name;
+            _age = age;
+            _pet = pet;
+        }
+
+        public void petActivate()
+        {
+            _pet.Poop();
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine(_name + " is eating...");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(_name + " is sleeping...");
+        }
+    }
+}
---
+++ OOPIntro/OOPIntro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOPIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet leffe = new Pet();
+            leffe._name = "Andreas";
+            leffe._species = "Dog";
+            leffe._color = "pink";
+
+            Person niklas = new Person("Niklas", 33, leffe);
+
+            niklas.Name = "gabbe";
+
+            niklas.Eat();
+            niklas.Sleep();
+            niklas.petActivate();
+        }
+    }
+}
